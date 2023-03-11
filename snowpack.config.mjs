6b98b3748713b353b12bd13dd26b3cbefb3aa9c9
David Garra <david.garra@ext.grupposandonato.it>
2023-03-11 17:34:22 +0100

    fix: clean snowpack.config

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -16,21 +16,4 @@ export default {
       },
     ],
   ],
-  routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
 };
