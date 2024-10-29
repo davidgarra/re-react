1543b197fdfabf3a387317fdc9a1a7facdd70aba
David Garra <53945399+davidgarra@users.noreply.github.com>
2024-10-29 14:08:59 +0100

    Remove useless comments

--- src/demo/index.tsx
+++ src/demo/index.tsx
@@ -1,17 +1,5 @@
 import { render, createElement } from '@re-react';
 import App from './App';
-// import App from './App';
-
-// const init = () => {
-//   const container = document.getElementById('root');
-//   if (container == null) return;
-
-//   render(App, container);
-// };
-
-// init();
-
-/** @jsx createElement */
 
 const element = <App />;
 const container = document.getElementById('root');
